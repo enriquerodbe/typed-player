463a40a7aeaaac22ac96de54fbb31dbf9cb58889
Enrique Rodríguez <enriquerodbe@gmail.com>
2020-05-29 18:53:00 -0300

    Privatize queue behavior trait

--- src/main/scala/com/typed/player/queue/QueueBehavior.scala
+++ src/main/scala/com/typed/player/queue/QueueBehavior.scala
@@ -3,7 +3,7 @@ package com.typed.player.queue
 import akka.actor.typed.Behavior
 import com.typed.player.queue.QueueCommands.QueueCommand
 
-trait QueueBehavior[QC <: QueueCommand] {
+private trait QueueBehavior[QC <: QueueCommand] {
 
   def receiveMessage(cmd: QC): Behavior[QueueCommand]
 }
